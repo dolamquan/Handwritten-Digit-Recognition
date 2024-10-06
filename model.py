355fccf9d106f3961ffbe6ff8999f85557ae14fd
dolamquan <143320449+dolamquan@users.noreply.github.com>
2024-10-06 10:44:27 -0700

    Update model.py

--- model.py
+++ model.py
@@ -19,20 +19,20 @@ x_test = tf.keras.utils.normalize(x_test,axis=1)
 
 
 #Loading and Training models
-#model = tf.keras.models.Sequential()
+model = tf.keras.models.Sequential()
 
 #Add flat layer
-#model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
 
-#Add dense layer
-#model.add(tf.keras.layers.Dense(128,activation = 'relu'))
-#model.add(tf.keras.layers.Dense(10,activation = 'softmax'))
+Add dense layer
+model.add(tf.keras.layers.Dense(128,activation = 'relu'))
+model.add(tf.keras.layers.Dense(10,activation = 'softmax'))
 
 
 
-#model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics = ['accuracy'])
-#model.fit(x_train,y_train, epochs= 3)
-#model.save('handwritten_model.keras')
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics = ['accuracy'])
+model.fit(x_train,y_train, epochs= 20)
+model.save('handwritten_model.keras')
 
 
 model = tf.keras.models.load_model('handwritten_model.keras')
